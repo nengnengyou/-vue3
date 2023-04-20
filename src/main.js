da76ef0de9565438f57cdbef387bb0a21c9bc9a1
nengnengyou <1936483000@qq.com>
2023-04-20 17:42:23 +0800

    拉拉

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "@/styles/index.scss";
+import SvgIcon from "@/icons";
+const app = createApp(App);
+SvgIcon(app);
 
-createApp(App).use(store).use(router).mount('#app')
+app.use(store).use(router).mount("#app");
---
+++ src/utils/request.js
@@ -0,0 +1,142 @@
+import axios from 'axios'
+import {
+  MessageBox,
+  Message
+} from 'element-ui'
+import store from '@/store'
+import {
+  getToken
+} from '@/utils/auth'
+
+// create an axios instance service就是axios，
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout  请求超时时间5s
+})
+
+// request interceptor 
+// 请求拦截器：再发请求之前，请求拦截器可以检测到，可以在请求发出去之前做一些事情
+service.interceptors.request.use(
+  config => {
+    // do something before request is sent
+
+    if (store.getters.token) {
+      // let each request carry token
+      // ['X-Token'] is a custom headers key
+      // please modify it according to the actual situation
+      config.headers['token'] = getToken()
+    }
+    return config
+  },
+  error => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  /**
+   * If you want to get http information such as headers or status
+   * Please return  response => response
+   */
+
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
+  response => {
+    const res = response.data
+
+    // if the custom code is not 20000, it is judged as an error.
+    if (res.code !== 1) {
+      Message({
+        message: res.msg || 'Error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+
+      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+        // to re-login
+        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
+          confirmButtonText: 'Re-Login',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
+      }
+      return Promise.reject(new Error(res.message || 'Error'))
+    } else {
+      console.log(response)
+      return response
+    }
+  },
+  error => {
+    console.log('err' + error) // for debug
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+// 发送请求
+export function post(url, params) {
+  // if (window.localStorage.getItem('mobile') == "" || window.localStorage.getItem('mobile') == null) {
+  //   window.localStorage.clear()
+  //   vm.$store.commit('clear')
+  // }
+
+  return new Promise((resolve, reject) => {
+    service
+      .post(url, params)
+      .then(
+        res => {
+          resolve(res.data)
+        },
+        err => {
+          reject(err.data)
+        }
+      )
+      .catch(err => {
+        reject(err.data)
+      })
+  })
+}
+
+
+export function get(url, params) {
+  // if (window.localStorage.getItem('mobile') == "" || window.localStorage.getItem('mobile') == null) {
+  //   window.localStorage.clear()
+  //   vm.$store.commit('clear')
+  // }
+
+  return new Promise((resolve, reject) => {
+    service
+      .get(url, {
+        params: params
+      })
+      .then(
+        res => {
+          resolve(res.data)
+        },
+        err => {
+          reject(err.data)
+        }
+      )
+      .catch(err => {
+        reject(err.data)
+      })
+  })
+}
+
+// export default service
